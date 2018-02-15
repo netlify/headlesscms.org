66fa9848f166b14d8883134e0beebdf80f4b54b1
Shawn Erquhart <shawn@erquh.art>
2018-02-15 11:00:17 -0500

    add github corner and share buttons

--- src/Header.js
+++ src/Header.js
@@ -1,35 +1,65 @@
 import React from 'react'
 import { Link } from 'react-static'
+import styled from 'styled-components'
 import logo from './images/headless-logo.svg'
+import { TwitterShareButton, TwitterIcon, RedditShareButton, RedditIcon } from 'react-share'
+import GitHubCorner from 'react-github-corner'
 
-const Header = ({}) =>
-  <div>
-    <div className="hero">
-      <h1>
-        <Link to="/" title="headlessCMS">
-          <img alt="headlessCMS" src={logo}/>
-        </Link>
-      </h1>
-      <h2>A List of Content Management Systems for JAMstack Sites</h2>
+const ShareButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
 
-      <div id="share-button-top" className='share-button share-button-top'></div>
+  > * {
+    margin: 0 6px;
+  }
+`
 
-      <a href="https://github.com/netlify/headlesscms.org">
-        <img style={{ position: 'absolute', top: 0, right: 0, border: 0 }} src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"/>
-      </a>
-    </div>
-    <div className="navbar">
-      <div className="container">
-        <div className="menu left">
-          <ul>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contribute">Contribute</Link></li>
-            <li><a href="https://jamstack.org/" target="_blank">What is JAMstack?</a></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
+const Header = () => {
+  const shareUrl = 'https://headlesscms.org'
+  const shareIconSize = '40'
+  const shareText = `
+headlessCMS is a leaderboard of the top Content Management Systems (CMS) for
+JAMstack sites. Promoting a static approach to building websites.
+  `
+  return (
+    <div>
+      <div className="hero">
+        <h1>
+          <Link to="/" title="headlessCMS">
+            <img alt="headlessCMS" src={logo}/>
+          </Link>
+        </h1>
+        <h2>A List of Content Management Systems for JAMstack Sites</h2>
+
+        <ShareButtonWrapper>
+          <TwitterShareButton url={shareUrl} title={shareText}>
+            <TwitterIcon size={shareIconSize} round={true}/>
+          </TwitterShareButton>
+          <RedditShareButton url={shareUrl} title={shareText}>
+            <RedditIcon size={shareIconSize} iconBgStyle={{ fill: '#ff4500' }} round={true}/>
+          </RedditShareButton>
+        </ShareButtonWrapper>
+
+        <GitHubCorner
+          href="https://github.com/netlify/headlesscms.org"
+          bannerColor="#313d3e"
+          size="100"
+        />
+      </div>
+      <div className="navbar">
+        <div className="container">
+          <div className="menu left">
+            <ul>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contribute">Contribute</Link></li>
+              <li><a href="https://jamstack.org/" target="_blank">What is JAMstack?</a></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  );
+}
 
 export default Header
