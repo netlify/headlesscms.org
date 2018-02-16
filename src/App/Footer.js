ec9877d58ed3fdca18be184cef3a298d02a823f7
Shawn Erquhart <shawn@erquh.art>
2018-02-16 13:31:58 -0500

    removed double Netlify shoutout from footer

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -7,7 +7,6 @@ const Footer = ({}) =>
         <h3>HeadlessCMS is hosted and maintained by <a href="https://www.netlify.com">Netlify</a>, the perfect way to deploy your JAMstack sites and apps.</h3>
       </div>
       <div className="postscript">
-        HeadlessCMS is hosted on <a href="https://www.netlify.com">Netlify</a>.<br/>
         © Netlify 2017
       </div>
     </div>
