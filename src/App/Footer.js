89425f9fcec956a766287b595608169fdb7a9b29
Shawn Erquhart <shawn@erquh.art>
2018-02-16 23:40:58 -0500

    fix copyright year

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Footer = ({}) =>
+const Footer = () =>
   <div>
     <div className="footer">
       <div className="footer-container">
         <h3>HeadlessCMS is hosted and maintained by <a href="https://www.netlify.com">Netlify</a>, the perfect way to deploy your JAMstack sites and apps.</h3>
       </div>
       <div className="postscript">
-        © Netlify 2017
+        © Netlify {new Date().getFullYear()}
       </div>
     </div>
   </div>
