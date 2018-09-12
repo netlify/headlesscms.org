e1137412ff83e924141fee5cd17cb7b004d14973
sw-yx <swyx@swyx.io>
2018-09-13 01:19:11 +0800

    init

--- src/App/Header.js
+++ src/App/Header.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import { Link, RouteData, Head } from 'react-static'
-import styled from 'styled-components'
-import { TwitterShareButton, TwitterIcon, RedditShareButton, RedditIcon } from 'react-share'
-import GitHubCorner from 'react-github-corner'
-import logo from 'Images/headless-logo.svg'
+import React from "react";
+import { Link, RouteData, Head } from "react-static";
+import styled from "styled-components";
+import {
+  TwitterShareButton,
+  TwitterIcon,
+  RedditShareButton,
+  RedditIcon
+} from "react-share";
+import GitHubCorner from "react-github-corner";
+import logo from "Images/headless-logo.svg";
 
 const ShareButtonWrapper = styled.div`
   display: flex;
@@ -12,20 +17,20 @@ const ShareButtonWrapper = styled.div`
   > * {
     margin: 0 4px;
   }
-`
+`;
 
 const ShareButton = styled(({ type, url, color, className, text }) => {
   const components = {
     twitter: { Button: TwitterShareButton, Icon: TwitterIcon },
-    reddit: { Button: RedditShareButton, Icon: RedditIcon },
-  }
+    reddit: { Button: RedditShareButton, Icon: RedditIcon }
+  };
   const { Button, Icon } = components[type];
 
   return (
     <Button url={url} title={text} className={className}>
-      <Icon size={40} round={true} iconBgStyle={{ fill: '#313d3e' }}/>
+      <Icon size={40} round={true} iconBgStyle={{ fill: "#313d3e" }} />
     </Button>
-  )
+  );
 })`
   cursor: pointer;
 
@@ -35,48 +40,110 @@ const ShareButton = styled(({ type, url, color, className, text }) => {
       fill: ${({ color }) => color} !important;
     }
   }
-`
+`;
 
-const Header = () =>
-  <RouteData render={({ title, shareUrl, shareText }) =>
-    <div>
-      <Head>
-        <title>
-          {`${title ? `${title} | headlessCMS` : 'headlessCMS | Top Content Management Systems for JAMstack sites'}`}
-        </title>
-      </Head>
-      <div className="hero">
-        <h1>
-          <Link to="/" title="headlessCMS">
-            <img alt="headlessCMS" src={logo}/>
-          </Link>
-        </h1>
-        <h2>A List of Content Management Systems for JAMstack Sites</h2>
+// swyx: temporary insert jamstack conf banner
+const JamstackConfBanner = () => (
+  <div className="promo-banner">
+    <p>
+      Learn more about the JAMstack at{" "}
+      <a href="https://jamstackconf.com">JAMstack Conf</a>, San Francisco —
+      29-30 October, 2018
+    </p>
+  </div>
+);
+const JamstackConfBannerStyled = styled(JamstackConfBanner)`
+  .promo-banner {
+    background-color: #000;
+    color: #fff;
+    display: block;
+    text-align: center;
+    z-index: 200;
+    position: fixed;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    p {
+      margin: 0;
+      padding-top: 0.6em;
+      padding-bottom: 0.6em;
+    }
+  }
+`;
+// swyx: temporary insert jamstack conf banner
 
-        <ShareButtonWrapper>
-          <ShareButton type="twitter" url={shareUrl} color="#1da1f2" text={shareText}/>
-          <ShareButton type="reddit" url={shareUrl} color="#ff4500" text={shareText}/>
-        </ShareButtonWrapper>
+const Header = () => (
+  <RouteData
+    render={({ title, shareUrl, shareText }) => (
+      <div>
+        <Head>
+          <title>
+            {`${
+              title
+                ? `${title} | headlessCMS`
+                : "headlessCMS | Top Content Management Systems for JAMstack sites"
+            }`}
+          </title>
+        </Head>
 
-        <GitHubCorner
-          href="https://github.com/netlify/headlesscms.org"
-          bannerColor="#313d3e"
-          size="100"
-        />
-      </div>
-      <div className="navbar">
-        <div className="container">
-          <div className="menu left">
-            <ul>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/contribute">Contribute</Link></li>
-              <li><a href="https://jamstack.org/" target="_blank">What is JAMstack?</a></li>
-              <li><Link to="/contact">Contact</Link></li>
-            </ul>
+        {/* swyx: temporary insert jamstack conf banner */}
+        <JamstackConfBannerStyled />
+        {/* swyx: temporary insert jamstack conf banner */}
+
+        <div className="hero">
+          <h1>
+            <Link to="/" title="headlessCMS">
+              <img alt="headlessCMS" src={logo} />
+            </Link>
+          </h1>
+          <h2>A List of Content Management Systems for JAMstack Sites</h2>
+
+          <ShareButtonWrapper>
+            <ShareButton
+              type="twitter"
+              url={shareUrl}
+              color="#1da1f2"
+              text={shareText}
+            />
+            <ShareButton
+              type="reddit"
+              url={shareUrl}
+              color="#ff4500"
+              text={shareText}
+            />
+          </ShareButtonWrapper>
+
+          <GitHubCorner
+            href="https://github.com/netlify/headlesscms.org"
+            bannerColor="#313d3e"
+            size="100"
+          />
+        </div>
+        <div className="navbar">
+          <div className="container">
+            <div className="menu left">
+              <ul>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/contribute">Contribute</Link>
+                </li>
+                <li>
+                  <a href="https://jamstack.org/" target="_blank">
+                    What is JAMstack?
+                  </a>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  }/>
+    )}
+  />
+);
 
-export default Header
+export default Header;
